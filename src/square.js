329f637de5fcf3330c4af3509edc3626181f6c92
jlisita <lisita_julien@hotmail.fr>
2024-06-14 17:38:53 +0200

    implemtation of Square class methods

--- src/square.js
+++ src/square.js
@@ -7,6 +7,8 @@ class Square {
         this.piece = null;
     }
 
+    // accessors
+
     setPiece(piece)
     {
         this.piece = piece;
@@ -22,24 +24,51 @@ class Square {
         this.row = row;
     }
 
+    getRow()
+    {
+        return this.row;
+    }
+
     setColumn(column)
     {
         this.column = column;
     }
 
+    getcolumn()
+    {
+        return this.column;
+    }
+
+    // class Methods
+
+    // remove the piece from the square and return it
     removePiece()
     {
-
+        let piece = this.piece;
+        this.piece = null;
+        return piece;
     }
 
+    // return true if there is a piece on this square, 
+    //otherwise return false
     isOccupied()
     {
-    
+        return this.getPiece != null;
     }
 
+    // place the piece on the square if it's empty and return true, 
+    // otherwise return false
     placePiece(piece)
     {
-
+        if(this.isOccupied())
+        {
+            return false;
+        }
+        else
+        {
+            this.piece = piece;
+            return true;
+        }
     }
 }
 
